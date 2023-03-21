e715ef64c0bda05e012961e87622da73ab43b28b
Rodrigo <rodrigo-zac@hotmail.com>
2023-03-20 22:13:19 -0300

    .

---
+++ NovoProjeto/Livro.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoProjeto
+{
+    internal class Livro
+    {
+        private int Id { get; set; }
+        private string Titulo { get; set; }
+        private string Autor { get; set; }
+        private string Editora { get; set; }
+        private int QuantidadeExemplares { get; set; }
+
+        public Livro(int id, string nome, string autor, string editora)
+        {
+            Id = id;
+            Nome = nome;
+            Autor = autor;
+            Editora = editora;
+        }
+
+        public void EmprestarLivro(int quantidadeEmprestada)
+        {
+            if (quantidadeEmprestada > 0 && quantidadeEmprestada <= QuantidadeExemplares)
+            {
+                QuantidadeExemplares -= quantidadeEmprestada;
+                Console.WriteLine($"Livro emprestado com sucesso! Restam {QuantidadeExemplares} exemplares.");
+            }
+            else
+            {
+                Console.WriteLine("Não é possível emprestar essa quantidade de exemplares.");
+            }
+        }
+
+        public void AlterarNomeLivro(string novoNome)
+        {
+            Nome = novoNome;
+        }
+
+        public void AlterarAutorLivro(string novoAutor)
+        {
+            Autor = novoAutor;
+        }
+
+        public string ObterNomeLivro()
+        {
+            return Nome;
+        }
+
+        public string ObterAutorLivro()
+        {
+            return Autor;
+        }
+
+        public int ObterIdLivro()
+        {
+            return Id;
+        }
+    }
+
+}
+
---
+++ NovoProjeto/Pessoa.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoProjeto
+{
+    public class Pessoa
+    {
+        int Id { get; set; }
+        string Nome { get; set; }
+        string Cpf { get; set; }
+        string Telefone { get; set; }
+        public object LivrosEmprestados { get; private set; }
+
+        List<string> livrosEmprestados = new List<string>();
+        
+
+
+        public void AdicionarLivroLista(Livro livro)
+        {
+            LivrosEmprestados.Add(livro);
+        }
+
+        public object GetLivrosEmprestados()
+        {
+            return LivrosEmprestados;
+        }
+
+        public void RemoverLivroLista(int idLivro, object livrosEmprestados)
+        {
+            Livro livro = livrosEmprestados.Find(l => l.Id == idLivro);
+            LivrosEmprestados.Remove(livro);
+        }
+        public void CadastrarPessoa()
+{
+         Console.WriteLine("Digite o Id da pessoa:");
+         int id = int.Parse(Console.ReadLine());
+
+    // Verifica se a pessoa já está cadastrada na lista de pessoas
+    if (listaPessoas.Any(p => p.Id == id))
+    {
+        Console.WriteLine("Pessoa já cadastrada");
+        return;
+    }
+
+    Console.WriteLine("Digite o nome da pessoa:");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("Digite o CPF da pessoa:");
+    string cpf = Console.ReadLine();
+
+    Console.WriteLine("Digite o telefone da pessoa:");
+    string telefone = Console.ReadLine();
+
+    // Cria um novo objeto Pessoa com as informações fornecidas e adiciona à lista de pessoas
+    Pessoa novaPessoa = new Pessoa()
+    {
+        Id = id,
+        Nome = nome,
+        Cpf = cpf,
+        Telefone = telefone
+    };
+    listaPessoas.Add(novaPessoa);
+
+    Console.WriteLine("Pessoa cadastrada com sucesso!");
+
+    // Zera a lista de livros emprestados da pessoa recém cadastrada
+    novaPessoa.LivrosEmprestados.Clear();
+
+    // Retorna ao menu principal
+    MostrarMenu();
+}
+    }
+}
